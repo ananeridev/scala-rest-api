69e2c92960845e6abcadc352ae00030fd0688ff3
anabneri <ananeri32@hotmail.com>
2020-10-06 20:33:39 -0300

    feat(controller): adding endpoints

---
+++ app/controllers/TodoController.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import javax.inject.Inject
+import models.Todo
+import play.api.libs.json.Json
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+class TodoController @Inject() (
+                               cc: ControllerComponents
+                               ) extends AbstractController(cc) {
+
+  implicit val todoFormat = Json.format[Todo]
+
+  def getAll = Action {
+    val todo = new Todo(1, "item 1", false)
+    Ok(Json.toJson(todo))
+  }
+
+}
